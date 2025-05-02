3da037ae9e4e2d9c61f91f57534e2a393c0f9cae
perseverance220 <perseverance220@gmail.com>
2025-05-02 19:49:03 +0900

    step1: rust up & hello_air ✅

---
+++ hello_air/target/debug/deps/hello_air.d
@@ -0,0 +1,5 @@
+C:\dev\air-playground\hello_air\target\debug\deps\hello_air.exe: src\main.rs
+
+C:\dev\air-playground\hello_air\target\debug\deps\hello_air.d: src\main.rs
+
+src\main.rs:
---
+++ hello_air/target/debug/hello_air.d
@@ -0,0 +1,1 @@
+C:\dev\air-playground\hello_air\target\debug\hello_air.exe: C:\dev\air-playground\hello_air\src\main.rs
